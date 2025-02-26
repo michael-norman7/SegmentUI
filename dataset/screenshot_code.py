6814289d2eb8f264764986ce6454174fbd578bbe
michael-norman7 <mnorman0801@gmail.com>
2025-02-26 14:45:07 -0600

    fixed and dedupped screenshot code

--- dataset/screenshot_code.py
+++ dataset/screenshot_code.py
@@ -1,56 +1,17 @@
 import asyncio
 import os
-
-# from utils import capture_screenshot
+import sys
 import base64
-import os
-from pyppeteer import launch
+
+# Add parent directory to path to import utils module
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils import capture_screenshot
 
 
 # Utility function to encode an image to base64
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
-
-
-async def capture_screenshot(html_path, output_image_path):
-    browser = await launch()
-    page = await browser.newPage()
-
-    await page.setViewport({"width": 1280, "height": 1000})
-
-    file_url = f"file:///{os.path.abspath(html_path)}"
-    await page.goto(file_url, {"waitUntil": "networkidle0"})
-
-    dimensions = await page.evaluate(
-        """() => {
-        return {
-            height: document.documentElement.offsetHeight,
-            width: document.documentElement.offsetWidth
-        }
-    }"""
-    )
-
-    await page.setViewport(
-        {"width": dimensions["width"], "height": dimensions["height"]}
-    )
-
-    await page.screenshot(
-        {
-            "path": output_image_path,
-            "fullPage": False,
-            "clip": {
-                "x": 0,
-                "y": 0,
-                "width": dimensions["width"],
-                "height": dimensions["height"],
-            },
-        }
-    )
-
-    print(f"Screenshot saved to {output_image_path}")
-
-    await browser.close()
 
 
 async def main():
